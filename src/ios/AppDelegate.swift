79069545fbd989b6ec20ca38a2c5cfae12e08318
Marcos Llerena Pisco <marcos_emilio7@hotmail.com>
2019-04-25 16:00:07 -0500

    add extension

--- src/ios/AppDelegate.swift
+++ src/ios/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
+extension AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
     var window: UIWindow?
     var locationManager: CLLocationManager!
