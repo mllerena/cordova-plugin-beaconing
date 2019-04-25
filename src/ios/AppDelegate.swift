a0b009702a552af954013b6824cd8661bb4838f9
Marcos Llerena Pisco <marcos_emilio7@hotmail.com>
2019-04-25 09:29:26 -0500

    add AppDelegate

--- src/ios/AppDelegate.swift
+++ src/ios/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 
 //@UIApplicationMain
-class AppDelegateBeaconsKeeFob: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
     var window: UIWindow?
     var locationManager: CLLocationManager!
