12e89fe6fb597f9b18ef1b2ad704f8947c69cb81
Marcos Llerena Pisco <marcos_emilio7@hotmail.com>
2019-04-26 17:15:51 -0500

    add implementation

---
+++ src/ios/AppDelegate+beacons.m
@@ -0,0 +1,40 @@
+#import "AppDelegate+beacons.h"
+#import <objc/runtime.h>
+
+@implementation AppDelegate (beacons)
+
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        Class class = [self class];
+
+        SEL originalSelector = @selector(application:didFinishLaunchingWithOptions:);
+        SEL swizzledSelector = @selector(xxx_application:didFinishLaunchingWithOptions:);
+        
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        
+        BOOL didAddMethod = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+        
+        if (didAddMethod) {
+            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+        
+    });
+}
+
+- (BOOL) xxx_application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    BOOL launchedWithoutOptions = launchOptions == nil;
+    
+    if (!launchedWithoutOptions) {
+        [self requestMoreBackgroundExecutionTime];
+    }
+    
+    return [self xxx_application:application didFinishLaunchingWithOptions:launchOptions];
+    
+}
