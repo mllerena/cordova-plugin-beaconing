eaeeab47443afff466de559aa646bdbf586e39da
Marcos Llerena Pisco <marcos_emilio7@hotmail.com>
2019-04-26 17:45:29 -0500

    Update AppDelegate+beacons.h

--- src/ios/AppDelegate+beacons.h
+++ src/ios/AppDelegate+beacons.h
@@ -1,6 +1,8 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate (beacons) <CLLocationManagerDelegate>
+  
+  @property (retain) CLLocationManager *locationManager;
 
 - (BOOL) xxx_application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
 
